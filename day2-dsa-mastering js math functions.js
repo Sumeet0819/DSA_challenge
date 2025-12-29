cba8c7e61d7064e49afbd8017eb61fe4adf80e38
Sumeet0819 <157307366+Sumeet0819@users.noreply.github.com>
2025-12-29 23:24:04 +0530

    day-1 commit

---
+++ day2-dsa-mastering js math functions.js
@@ -0,0 +1,30 @@
+// Compound Interest
+// let p = 1000
+// let r =5
+// let t = 3
+// console.log((p*Math.pow(1+r/100,t))-p);
+
+// otp generator
+// console.log(Math.floor(Math.random()*9000+1000));
+
+// Herons formula
+
+// let a = 5
+// let b = 6
+// let c = 7
+
+// let s = (a+b+c)/2
+
+// if(a+b<=c || b+c<=a || c+a<=b){
+//     console.log("Invalid triangle");
+// }
+// else{
+//     console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+// }
+
+
+
+// Circumference of circle
+// let r = 5
+// console.log(2*Math.PI*r);
+
