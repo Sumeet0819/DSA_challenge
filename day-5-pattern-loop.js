b3f4842dad16926cc80c054bf52884cf6b70d607
Sumeet0819 <157307366+Sumeet0819@users.noreply.github.com>
2026-01-20 22:48:53 +0530

    day-5 phase 2

--- day-5-pattern-loop.js
+++ day-5-pattern-loop.js
@@ -1,6 +1,6 @@
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-let n = prompt("enter a number ")
+let n = Number(prompt("enter a number "))
 
 // for(let i = 1; i <= n; i++) {
 //     for(let j=1; j<=i;j++){
@@ -9,30 +9,75 @@ let n = prompt("enter a number ")
 //         console.log();
 // }
 
-
 // for(let i =1; i<=n;i++){
 //     for(let j=1;j<=i;j++){
 //         process.stdout.write(j+" ")
 //     }
-//     console.log(); 
+//     console.log();
 // }
 
 // for(let i =1; i<=n;i++){
 //     for(let j=1;j<=i;j++){
 //         process.stdout.write(String.fromCharCode(64 + j))
 //     }
-//     console.log(); 
+//     console.log();
 // }
-
 
 // inverted right angled triangle
 
-for(let i =1; i<=n;i++){
-    for(let j=1;j<=n-i+1;j++){
-        process.stdout.write("*")
+// for(let i =1; i<=n;i++){
+//     for(let j=1;j<=n-i+1;j++){
+//         process.stdout.write("*")
+//     }
+//     console.log();
+// }
+
+// mirrored right-angled triangle
+
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n-i;j++){
+//         process.stdout.write(" ");
+//     }
+//     for(let j =1;j<=i;j++){
+//         process.stdout.write("*")
+//     }
+//     console.log();
+// }
+
+//
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n-i;j++){
+//         process.stdout.write(" ");
+//     }
+//     for(let j =1;j<=i;j++){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+// X pattern
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if(i==j || i+j ==n+1){
+//         process.stdout.write("* ")
+//     }
+//     else{
+//         process.stdout.write(' ')
+//     }
+//   }
+//   console.log();
+// }
+
+//  V pattern
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= (n*2)-1; j++) {
+    if(i==j || i+j ==n*2){
+        process.stdout.write("* ")
     }
-    console.log(); 
+    else{
+        process.stdout.write(' ')
+    }
+  }
+  console.log();
 }
-
-
-
